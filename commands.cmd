682f6ac44d06d5068b1051d6ffdf89e827e7c1fb
habibafullstackdev <habiba.iqbal272@gmail.com>
2024-11-24 14:46:10 +0500

    Finished todyas lession with themes.

---
+++ commands.cmd
@@ -0,0 +1,36 @@
+1. Install python
+2. make a new python environment
+python -m venv venv
+
+3. Open the environment
+
+cd venv
+ls
+Include  Lib  pyvenv.cfg  Scripts
+
+cd scripts
+
+ls
+activate      activate.fish  deactivate.bat  pip3.13.exe  python.exe
+activate.bat  Activate.ps1   pip.exe         pip3.exe     pythonw.exe
+
+activate.bat
+
+windows method
+
+4. install mkdocs-material
+pip install mkdocs-material
+
+5. Start new mkdocs project
+mkdocs new .
+
+6. Lanunch the site
+mkdocs serve
+
+7. install yaml plugin
+
+8. Change vscode setting to allow error high lighting. Take code from the site bellow and past it in the setting.json file
+https://squidfunk.github.io/mkdocs-material/creating-your-site/
+
+
+
